3833f817b0a2cd6d119b588b32eefda1405252c5
Katelyn Pozon <katelyn.pozon98@gmail.com>
2019-03-28 22:21:17 -0700

    Tried to connect text fields to firebase

--- Somnus/LoginViewController.swift
+++ Somnus/LoginViewController.swift
@@ -9,3 +9,29 @@
 import Foundation
 import UIKit
 
+class LoginViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+//        Auth.auth().signIn(withEmail: email.text!, password: password.text!) { (user, error) in
+//            if error == nil{
+//                self.performSegue(withIdentifier: "loginToHome", sender: self)
+//            }
+//            else{
+//                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+//                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+//
+//                alertController.addAction(defaultAction)
+//                self.present(alertController, animated: true, completion: nil)
+//            }
+//        }
+//
+//    }
+//    }
+}
+}
--- Somnus/SignUpViewController.swift
+++ Somnus/SignUpViewController.swift
@@ -8,3 +8,30 @@
 
 import Foundation
 import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+//    @IBAction func buttonTapped(_ sender: UIButton) {
+//        Auth.auth().createUser(withEmail: email.text!, password: password.text!) { (user, error) in
+//            if error == nil {
+//                self.performSegue(withIdentifier: "signupToHome", sender: self)
+//            } else {
+//                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+//                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+//
+//                alertController.addAction(defaultAction)
+//                self.present(alertController, animated: true, completion: nil)
+//            }
+//
+//    }
+//
+//}
+}
